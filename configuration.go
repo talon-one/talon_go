0e8ff56cb6ac428e7af7035ba994aba7cb389f7e
Jakob (Koby) Shimony <shimony@talon.one>
2020-06-05 14:54:41 +0200

    Initial Commit

--- configuration.go
+++ configuration.go
@@ -97,7 +97,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "OpenAPI-Generator/2.1.0/go",
+		UserAgent:     "OpenAPI-Generator/2.1.1/go",
 		Debug:         false,
 		Servers: ServerConfigurations{
 			{
